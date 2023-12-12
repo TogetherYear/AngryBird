43a4f99ca99c0d2b4d1eec7aba498dbf9f0f9af3
zhangwenjun <1346502061@qq.com>
2023-12-12 17:46:22 +0800

    Update:Score

---
+++ Assets/Scripts/Slingshot/TScorePopup.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TScorePopup : TSingleton<TScorePopup>
+{
+    protected override void Awake()
+    {
+        base.Awake();
+    }
+}
