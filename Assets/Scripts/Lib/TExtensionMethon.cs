a513ec93a576f05d110fa751a839f4fddaccecde
zhangwenjun <1346502061@qq.com>
2023-12-18 16:15:24 +0800

    Update:TExtensionTool

--- Assets/Scripts/Lib/TExtensionMethon.cs
+++ Assets/Scripts/Lib/TExtensionMethon.cs
@@ -24,16 +24,3 @@ public static class TExtensionMethon
         return position;
     }
 }
-
-public class TExtensionTool
-{
-    public static bool IsUserHold()
-    {
-        return Input.touchCount != 0 || Input.GetMouseButtonDown(0);
-    }
-
-    public static Vector2 GetUserHoldPosition()
-    {
-        return Input.touchCount != 0 ? Input.GetTouch(0).position : Input.mousePosition;
-    }
-}
