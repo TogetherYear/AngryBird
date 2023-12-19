d8e989ce9534c14a68c0b9ffb4d4e4d03b272f17
zhangwenjun <1346502061@qq.com>
2023-12-19 10:54:00 +0800

    Update:TExplodeBird

--- Assets/Scripts/Bird/TExplodeBird.cs
+++ Assets/Scripts/Bird/TExplodeBird.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class TExplodeBird : TBird
 {
+    [SerializeField]
+    private float boomRadius = 2.5f;
+
+    [SerializeField]
+    private float forceScale = 20.0f;
     protected override void FlyingSkillControl()
     {
         base.FlyingSkillControl();
@@ -12,5 +17,15 @@ public class TExplodeBird : TBird
     protected override void CollisionSkillControl()
     {
         base.CollisionSkillControl();
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(transform.position, boomRadius);
+        foreach (var c in colliders)
+        {
+            TDestructible d = c.GetComponent<TDestructible>();
+            if (d)
+            {
+                Rigidbody2D rb = d.GetComponent<Rigidbody2D>();
+                rb.AddForce((c.transform.position - transform.position).normalized * forceScale, ForceMode2D.Impulse);
+            }
+        }
     }
 }
