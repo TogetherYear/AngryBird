d7d1da1ec04eaea9988e91c5916d8be2af6389a3
zhangwenjun <1346502061@qq.com>
2023-12-22 11:10:54 +0800

    Update:FinishUI

--- Assets/Scripts/UI/TFinishUI.cs
+++ Assets/Scripts/UI/TFinishUI.cs
@@ -6,4 +6,38 @@ public class TFinishUI : MonoBehaviour
 {
     [SerializeField]
     private Animator at;
+
+    [SerializeField]
+    private CanvasGroup leftStart;
+
+    [SerializeField]
+    private CanvasGroup midStart;
+
+    [SerializeField]
+    private CanvasGroup rightStart;
+
+    public void Success()
+    {
+        at.SetBool("Success", true);
+    }
+
+    public void Failed()
+    {
+        at.SetBool("Success", false);
+    }
+
+    public void OnRefreshClick()
+    {
+
+    }
+
+    public void OnLevelClick()
+    {
+
+    }
+
+    public void OnNextClick()
+    {
+
+    }
 }
